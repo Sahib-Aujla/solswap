16a6a87556ab27391ad3cf513af13f63a4f716a9
Sahib.Aujla <sahibpreet568@gmail.com>
2025-02-11 19:22:09 -0500

    added main component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import Main from "@/components/Main";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div className="w-[100vw] h-[100vh] bg-gray-950 text-white">
       <Navbar />
+      <div className=" max-h-full max-w-full flex justify-center items-center">
+        <div className="w-1/4 h-1/4">
+          <Main />
+        </div>
+      </div>
     </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,10 @@
-import React from 'react'
-
 const Navbar = () => {
   return (
-    <div className='flex justify-between p-5'>
-        <div>
-            SolSwap
-        </div>
-        <div>
-            Wallet connect logic
-        </div>
+    <div className="flex justify-between p-5">
+      <div>SolSwap</div>
+      <div>Wallet connect logic</div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
