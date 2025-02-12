a7391bc356ef2c356004a6952de2ca01880e0e68
Sahib.Aujla <sahibpreet568@gmail.com>
2025-02-11 19:52:39 -0500

    added logic to connect a wallet

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,13 @@
+'use client'
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
 const Navbar = () => {
   return (
     <div className="flex justify-between p-5">
       <div>SolSwap</div>
-      <div>Wallet connect logic</div>
+      <div>
+        <WalletMultiButton />
+      </div>
     </div>
   );
 };
