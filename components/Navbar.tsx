3ff6960354ad1a5675ac4e36177523174708683a
Sahib.Aujla <sahibpreet568@gmail.com>
2025-02-11 19:11:58 -0500

    added navbar and solana deps

---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between p-5'>
+        <div>
+            SolSwap
+        </div>
+        <div>
+            Wallet connect logic
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
