c0708ef3fe54044a67d3d94542f589990b265066
Sahib.Aujla <sahibpreet568@gmail.com>
2025-02-17 07:49:01 -0500

    added swap functions

--- app/(main)/swap/page.tsx
+++ app/(main)/swap/page.tsx
@@ -1,11 +1,60 @@
-import { getAllTokens } from "@/utils/jup";
-import React, { useEffect } from "react";
+"use client";
+import { getQuote } from "@/utils/jup";
+import { QuoteResponse } from "@jup-ag/api";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import React, { useState } from "react";
 
 const Swap = () => {
-  useEffect(() => {
-    getAllTokens().then((res) => console.log(res));
-  }, []);
-  return <div></div>;
+  const wallet = useWallet();
+  const [val, setVal] = useState("0");
+  const [val2, setVal2] = useState("0");
+  const [quote, setQuote] = useState<QuoteResponse>();
+
+  const createTxn = async () => {
+    if (!wallet || !wallet.publicKey || !quote) return;
+
+  };
+  return (
+    <div>
+      <div className="border border-slate-300 rounded-lg p-4">
+        <h2 className="text-xl font-bold mb-4">Swap Sol for USDC</h2>
+        <div className="flex flex-col gap-2">
+          <input
+            value={val}
+            onChange={async (e) => {
+              setVal(e.target.value);
+
+              const lmp = parseFloat(e.target.value) * LAMPORTS_PER_SOL;
+              console.log({ lmp });
+              if (lmp === 0 || isNaN(lmp)) return;
+              const resp = await getQuote(lmp);
+              setVal2(resp.outAmount);
+              setQuote(quote);
+            }}
+            type="text"
+            placeholder="Sol Value"
+            className="bg-inherit border border-slate-500 p-2"
+          />
+          <input
+            value={val2}
+            onChange={(e) => {
+              console.log(e.target.value);
+            }}
+            type="text"
+            placeholder="USDC value"
+            className="bg-inherit border border-slate-500 p-2"
+          />
+          <button
+            onClick={createTxn}
+            className="bg-white text-black p-3 mt-2 hover:bg-slate-100"
+          >
+            Swap
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Swap;
--- utils/jup.ts
+++ utils/jup.ts
@@ -1,8 +1,38 @@
-import { createJupiterApiClient } from "@jup-ag/api";
+import { createJupiterApiClient, QuoteResponse } from "@jup-ag/api";
 
-const jupClient=createJupiterApiClient();
+const jupClient = createJupiterApiClient();
 
-export async function getAllTokens(){
-    const resp=await jupClient.tokensGet();
+export async function getAllTokens() {
+    const resp = await jupClient.tokensGet();
     return resp;
+}
+
+export async function getQuote(amount: number) {
+    const res = await jupClient.quoteGet({
+        inputMint: "So11111111111111111111111111111111111111112",
+        outputMint: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+        amount: amount,
+
+    })
+    return res;
+}
+
+export async function getSwapResponse(publicKey: string, quote: QuoteResponse) {
+    // Get serialized transaction
+
+    const swapResponse = await jupClient.swapPost({
+        swapRequest: {
+            quoteResponse: quote,
+            userPublicKey: publicKey,
+            dynamicComputeUnitLimit: true,
+            prioritizationFeeLamports: {
+                priorityLevelWithMaxLamports: {
+                    maxLamports: 10000000,
+                    priorityLevel: "veryHigh", // If you want to land transaction fast, set this to use `veryHigh`. You will pay on average higher priority fee.
+                },
+            },
+            correctLastValidBlockHeight: true,
+        },
+    });
+    return swapResponse;
 }
